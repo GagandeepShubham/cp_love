0084ba458d518fec0ebbcc9a4b53d873645d7fe1
GagandeepShubham <prabhakar.gagan21@gmail.com>
2020-11-03 11:08:39 +0530

    Program to perform the union and intersection of two sorted arrays.

--- union_intersect_array.cpp
+++ union_intersect_array.cpp
@@ -1,5 +1,8 @@
 //Problem 6: Array
 //Find Union and intersection of two sorted arrays.
+//I am using the merge procedure which we use in a mergesort algorithm to merge two sorted
+//arrays.
+//Time complexity: O(m+n), m and n size of the arrays.
 //Date : 02/11/2020
 #include<iostream>
 #include<vector>
@@ -29,93 +32,95 @@ int main()
   }
   
 
-  //Merge the arrays:
+  //Union of two arrays using the merge procedure
   i = 0, j = 0;
   while(true)
   {
-       if(i == a.size())break;
-       if(j == b.size())break;
+       if(i == a.size())break; //if the i reaches to the end of first array then exit while loop
+       if(j == b.size())break; //if the j reaches to the end of second array then exit while loop
 
-       if(a[i] < b[j])
+       if(a[i] < b[j]) //if ith element of a is lesser then the jth element of b
        {
-            res.push_back(a[i]);
-            i++;
+            res.push_back(a[i]); //push the smaller element in the res array
+            i++;                 //increment the i index to consider the next element of array a
        }
-       else if(a[i] > b[j])
+       else if(a[i] > b[j]) //if the jth element of the b is smaller than the ith element of a
        {
-            res.push_back(b[j]);
-            j++;
+            res.push_back(b[j]); //push the smaller element in the result array.
+            j++;                 //increment the j index to consider the next element of array b
         }
-       else if(a[i] == b[j])
+       else if(a[i] == b[j]) //if the both elements are equal then push both of them (union)
        {
             res.push_back(a[i]);
             res.push_back(b[j]);
-            i++;
-            j++;
+            i++;             //increment i to consider next element of a
+            j++;             //increment j to consider next element of b
         }
     }
 
 
 
-    while(i < a.size())
+    //Only one of the following while loops will execute
+    while(i < a.size())  // if some elements are still remaining in a then push all of them in res
     {
         res.push_back(a[i]);
         i++;
     }
 
-    while(j < b.size())
+    while(j < b.size()) //if some elements are still remaining in b then push all of them in res.
     {
         res.push_back(b[j]);
         j++;
     }
 
     cout << "\nUnion of the arrays: ";
-    for(int x: res)cout << x << " ";
+    for(int x: res)cout << x << " "; //printing the result of union of the arrays.
     cout << "\n";
 
 
     //Merge procedure for intersecting the arrays:
-    res.clear();
+    res.clear();  
     i = 0; j = 0;
     while(true)
     {
-      if(i == a.size())break;
-      if(j == b.size())break;
+      if(i == a.size())break; //if i reaches end then break because all elements of a would've been compared.
+      if(j == b.size())break; //if j reaches end then break because all elements of b would've been compared.
 
-      if(a[i] == res[res.size() - 1])i++;
-      if(b[j] == res[res.size() - 1])j++;
-      if(a[i] < b[j]) 
+      if(a[i] == res[res.size() - 1])i++; //if the current element of a matches the last element in res then skip the a[i].
+      if(b[j] == res[res.size() - 1])j++; //if the current element of b matches the last element in res then skip the b[i].
+      if(a[i] < b[j])  //if the current element of a is smaller than the current element of b
       {
-        res.push_back(a[i]);
-        i++;
+        res.push_back(a[i]); //push the smaller element in res
+        i++;                 //increment i to consider next element of a.
       }
-      else if(a[i] > b[j]) 
+      else if(a[i] > b[j])  // if the current element of b is smaller than the current element of a.
       {
-        res.push_back(b[j]);
-        j++;
+        res.push_back(b[j]); //push the smaller element in res.
+        j++;                 //increment j to consider next element of b.
       }
-      else if(a[i] == b[j])
+      else if(a[i] == b[j]) //if both the current elements, a[i] and b[j] are equal then push only one of them.
       {
-        res.push_back(a[i]);
-        i++;
-        j++;
+        res.push_back(a[i]); //push either a[i] or b[j], both are same.
+        i++;                 //increment i to consider next element of a.
+        j++;                 //increment j to consider next element of b.
       }
     }
 
-    while(i < a.size())
+    //only one of the while loop will execute.
+    while(i < a.size()) //if some elements are still remaining in a then push them all in the res array.
     {
       res.push_back(a[i]);
       i++;
     }
 
-    while(j < b.size())
+    while(j < b.size()) //if some elements are still remaining in b then push them all in the res array.
     {
       res.push_back(b[j]);
       j++;
     }
 
     cout << "\nIntersection of the arrays: ";
-    for(int x: res)cout << x << " ";
+    for(int x: res)cout << x << " ";                //printing the result of intersection.
     cout << "\n";
 
 }
